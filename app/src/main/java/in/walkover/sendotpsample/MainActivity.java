e0bd244be704673ea436ae5974e486edc051f520
Rajendra Verma <rajendrarajaramv@gmail.com>
2019-04-30 19:29:44 +0530

    Update MainActivity.java

--- app/src/main/java/in/walkover/sendotpsample/MainActivity.java
+++ app/src/main/java/in/walkover/sendotpsample/MainActivity.java
@@ -1,4 +1,4 @@
-package in.walkover.sendotpsample;
+package com.msg91.sendotp.sample;
 
 import android.Manifest;
 import android.content.Context;
@@ -16,6 +16,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+
 import com.msg91.sendotpandroid.library.PhoneNumberFormattingTextWatcher;
 import com.msg91.sendotpandroid.library.PhoneNumberUtils;
 import com.msg91.sendotpandroid.library.internal.Iso2Phone;
@@ -24,117 +25,117 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-  public static final String INTENT_PHONENUMBER = "phonenumber";
-  public static final String INTENT_COUNTRY_CODE = "code";
+    public static final String INTENT_PHONENUMBER = "phonenumber";
+    public static final String INTENT_COUNTRY_CODE = "code";
 
-  private EditText mPhoneNumber;
-  private Button mSmsButton;
-  private String mCountryIso;
-  private TextWatcher mNumberTextWatcher;
+    private EditText mPhoneNumber;
+    private Button mSmsButton;
+    private String mCountryIso;
+    private TextWatcher mNumberTextWatcher;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-    setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
-    mPhoneNumber = (EditText) findViewById(R.id.phoneNumber);
-    mSmsButton = (Button) findViewById(R.id.smsVerificationButton);
+        mPhoneNumber = (EditText) findViewById(R.id.phoneNumber);
+        mSmsButton = (Button) findViewById(R.id.smsVerificationButton);
 
-    mCountryIso = PhoneNumberUtils.getDefaultCountryIso(this);
-    final String defaultCountryName = new Locale("", mCountryIso).getDisplayName();
-    final CountrySpinner spinner = (CountrySpinner) findViewById(R.id.spinner);
-    spinner.init(defaultCountryName);
-    spinner.addCountryIsoSelectedListener(new CountrySpinner.CountryIsoSelectedListener() {
-      @Override
-      public void onCountryIsoSelected(String selectedIso) {
-        if (selectedIso != null) {
-          mCountryIso = selectedIso;
-          resetNumberTextWatcher(mCountryIso);
-          // force update:
-          mNumberTextWatcher.afterTextChanged(mPhoneNumber.getText());
-        }
-      }
-    });
-    resetNumberTextWatcher(mCountryIso);
+        mCountryIso = PhoneNumberUtils.getDefaultCountryIso(this);
+        final String defaultCountryName = new Locale("", mCountryIso).getDisplayName();
+        final CountrySpinner spinner = (CountrySpinner) findViewById(R.id.spinner);
+        spinner.init(defaultCountryName);
+        spinner.addCountryIsoSelectedListener(new CountrySpinner.CountryIsoSelectedListener() {
+            @Override
+            public void onCountryIsoSelected(String selectedIso) {
+                if (selectedIso != null) {
+                    mCountryIso = selectedIso;
+                    resetNumberTextWatcher(mCountryIso);
+                    // force update:
+                    mNumberTextWatcher.afterTextChanged(mPhoneNumber.getText());
+                }
+            }
+        });
+        resetNumberTextWatcher(mCountryIso);
 
-    tryAndPrefillPhoneNumber();
-  }
-
-  private void tryAndPrefillPhoneNumber() {
-    if (checkCallingOrSelfPermission(Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
-      TelephonyManager manager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-      mPhoneNumber.setText(manager.getLine1Number());
-    } else {
-      ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, 0);
-    }
-  }
-
-  public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-    if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-      tryAndPrefillPhoneNumber();
-    } else {
-      if (ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[0])) {
-        Toast.makeText(this, "This application needs permission to read your phone number to automatically "
-            + "pre-fill it", Toast.LENGTH_LONG).show();
-      }
-    }
-  }
-
-  private void openActivity(String phoneNumber) {
-    Intent verification = new Intent(this, VerificationActivity.class);
-    verification.putExtra(INTENT_PHONENUMBER, phoneNumber);
-    verification.putExtra(INTENT_COUNTRY_CODE, Iso2Phone.getPhone(mCountryIso));
-    startActivity(verification);
-  }
-
-  private void setButtonsEnabled(boolean enabled) {
-    mSmsButton.setEnabled(enabled);
-  }
-
-  public void onButtonClicked(View view) {
-    openActivity(getE164Number());
-  }
-
-  private void resetNumberTextWatcher(String countryIso) {
-
-    if (mNumberTextWatcher != null) {
-      mPhoneNumber.removeTextChangedListener(mNumberTextWatcher);
+        tryAndPrefillPhoneNumber();
     }
 
-    mNumberTextWatcher = new PhoneNumberFormattingTextWatcher(countryIso) {
-      @Override
-      public void onTextChanged(CharSequence s, int start, int before, int count) {
-        super.onTextChanged(s, start, before, count);
-      }
-
-      @Override
-      public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-        super.beforeTextChanged(s, start, count, after);
-      }
-
-      @Override
-      public synchronized void afterTextChanged(Editable s) {
-        super.afterTextChanged(s);
-        if (isPossiblePhoneNumber()) {
-          setButtonsEnabled(true);
-          mPhoneNumber.setTextColor(Color.BLACK);
+    private void tryAndPrefillPhoneNumber() {
+        if (checkCallingOrSelfPermission(Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
+            TelephonyManager manager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+            mPhoneNumber.setText(manager.getLine1Number());
         } else {
-          setButtonsEnabled(false);
-          mPhoneNumber.setTextColor(Color.RED);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, 0);
         }
-      }
-    };
+    }
 
-    mPhoneNumber.addTextChangedListener(mNumberTextWatcher);
-  }
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            tryAndPrefillPhoneNumber();
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[0])) {
+                Toast.makeText(this, "This application needs permission to read your phone number to automatically "
+                        + "pre-fill it", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 
-  private boolean isPossiblePhoneNumber() {
-    return PhoneNumberUtils.isPossibleNumber(mPhoneNumber.getText().toString(), mCountryIso);
-  }
+    private void openActivity(String phoneNumber) {
+        Intent verification = new Intent(this, VerificationActivity.class);
+        verification.putExtra(INTENT_PHONENUMBER, phoneNumber);
+        verification.putExtra(INTENT_COUNTRY_CODE, Iso2Phone.getPhone(mCountryIso));
+        startActivity(verification);
+    }
 
-  private String getE164Number() {
-    return mPhoneNumber.getText().toString().replaceAll("\\D", "").trim();
-    // return PhoneNumberUtils.formatNumberToE164(mPhoneNumber.getText().toString(), mCountryIso);
-  }
+    private void setButtonsEnabled(boolean enabled) {
+        mSmsButton.setEnabled(enabled);
+    }
+
+    public void onButtonClicked(View view) {
+        openActivity(getE164Number());
+    }
+
+    private void resetNumberTextWatcher(String countryIso) {
+
+        if (mNumberTextWatcher != null) {
+            mPhoneNumber.removeTextChangedListener(mNumberTextWatcher);
+        }
+
+        mNumberTextWatcher = new PhoneNumberFormattingTextWatcher(countryIso) {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                super.onTextChanged(s, start, before, count);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                super.beforeTextChanged(s, start, count, after);
+            }
+
+            @Override
+            public synchronized void afterTextChanged(Editable s) {
+                super.afterTextChanged(s);
+                if (isPossiblePhoneNumber()) {
+                    setButtonsEnabled(true);
+                    mPhoneNumber.setTextColor(Color.BLACK);
+                } else {
+                    setButtonsEnabled(false);
+                    mPhoneNumber.setTextColor(Color.RED);
+                }
+            }
+        };
+
+        mPhoneNumber.addTextChangedListener(mNumberTextWatcher);
+    }
+
+    private boolean isPossiblePhoneNumber() {
+        return PhoneNumberUtils.isPossibleNumber(mPhoneNumber.getText().toString(), mCountryIso);
+    }
+
+    private String getE164Number() {
+        return mPhoneNumber.getText().toString().replaceAll("\\D", "").trim();
+        // return PhoneNumberUtils.formatNumberToE164(mPhoneNumber.getText().toString(), mCountryIso);
+    }
 }
