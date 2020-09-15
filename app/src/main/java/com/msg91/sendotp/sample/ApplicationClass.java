2aa5a9ca73c0a265f66ecc4b876ed0873acccc62
richa <richa@walkover.in>
2020-09-15 11:45:52 +0530

    Bug fixing and resolve issue using proguard

--- app/src/main/java/com/msg91/sendotp/sample/ApplicationClass.java
+++ app/src/main/java/com/msg91/sendotp/sample/ApplicationClass.java
@@ -8,6 +8,6 @@ public class ApplicationClass extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        SendOTP.initializeApp(this,"SEND_OTP_KEY");
+        SendOTP.initializeApp(this,"SEND_OTP_KEY:authKey");
     }
 }
